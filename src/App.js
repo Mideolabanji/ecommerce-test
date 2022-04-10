897ea8cfe3dbe28b8f0e47fea7104fb1ce78beb3
Mideolabanji <89583482+Mideolabanji@users.noreply.github.com>
2022-04-10 14:58:27 +0100

    installed dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import SignUp from "./SignUp";
 
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      Test <SignUp />
+    </div>
+  );
 }
 
 export default App;
